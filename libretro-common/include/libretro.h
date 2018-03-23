467c2626d6c7cc5296d1f55c4a75dd16e16ae4f8
Dwedit <danweiss@gmail.com>
2018-03-23 16:35:52 -0500

    Added one line to the documentation.

--- libretro-common/include/libretro.h
+++ libretro-common/include/libretro.h
@@ -1116,7 +1116,8 @@ struct retro_led_interface
 #define RETRO_ENVIRONMENT_GET_AUDIO_VIDEO_ENABLE (47 | RETRO_ENVIRONMENT_EXPERIMENTAL)
                                            /* int * --
                                             * Queries the frontend if audio and video are enabled or not.
-                                            * If not enabled, the frontend will discard the audio or video.
+                                            * If not enabled, the frontend will discard the audio or video,
+                                            * so the core may decide to skip producing audio or video.
                                             * Bit 0 (value 1) is set if Video is enabled,
                                             * Bit 1 (value 2) is set if Audio is enabled.
                                             * Other bits are reserved for future use.
